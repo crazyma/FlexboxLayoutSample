02b846e1b30036000fb3d21be673dace491569eb
Badu <badu@dcard.cc>
2018-12-05 17:11:30 +0800

    feat: insert sample item

--- app/src/main/java/com/crazyma/flexboxlayoutsample/MainActivity.kt
+++ app/src/main/java/com/crazyma/flexboxlayoutsample/MainActivity.kt
@@ -2,11 +2,33 @@ package com.crazyma.flexboxlayoutsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        insertItem()
+    }
+
+    private fun insertItem() {
+        for (i in 0..16) {
+            (Math.random() * 10000).toInt().toString().run {
+                (LayoutInflater.from(this@MainActivity)
+                    .inflate(R.layout.view_flex_item, flexboxLayout, false) as TextView).apply {
+                    text = this@run
+                    isSelected = false
+                    setOnClickListener {
+
+                    }
+                }.apply {
+                    flexboxLayout.addView(this)
+                }
+            }
+        }
     }
 }
